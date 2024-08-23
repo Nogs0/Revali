46e5d2daae3ed68478b96ad76c49ec4a5d70b089
Nogs0 <joaoguinogueira04@gmail.com>
2024-08-23 00:19:16 -0300

    ajustando tela de login - em andamento

--- app/revali/src/app/(auth)/index.tsx
+++ app/revali/src/app/(auth)/index.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Colors } from '@/constants/Colors'
+import { useAuthContext } from '@/src/contexts/authContext'
 
 export default function Login() {
+
+  const { login } = useAuthContext();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView style={{ height: '100%' }}>
+      <View style={{ height: '30%', alignItems: 'center', justifyContent: 'center' }}>
+        <Image source={require('@/assets/images/logo-banco.png')} />
+      </View>
+      <View style={{ backgroundColor: Colors.lime900, height: '70%', paddingHorizontal: '5%', paddingTop: 30 }}>
+        <TextInput
+          placeholder='Email'
+          style={{ backgroundColor: 'white', height: 40, marginHorizontal: 20, marginVertical: 10, borderRadius: 15 }}
+          onChangeText={setEmail}
+          value={email}
+        ></TextInput>
+        <TextInput
+          placeholder='Senha'
+          style={{ backgroundColor: 'white', height: 40, marginHorizontal: 20, marginVertical: 10, borderRadius: 15 }}
+          onChangeText={setPassword}
+          value={password}
+          ></TextInput>
+        <TouchableOpacity style={{ marginTop: 50, backgroundColor: Colors.lime500, borderRadius: 15, marginHorizontal: 50, height: 30, alignItems: 'center', justifyContent: 'center' }} onPress={() => login(email, password)}>
+          <Text>ENTRAR</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
--- app/revali/src/contexts/authContext.tsx
+++ app/revali/src/contexts/authContext.tsx
@@ -2,19 +2,25 @@ import { createContext, useContext, useState } from "react";
 
 interface AuthContextData {
     signed: boolean,
-    userId: number
+    userId: number | undefined,
+    login(email: string | undefined, password: string | undefined): void
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: any) {
-    const [userId, setUserId] = useState<number>(1);
+    const [userId, setUserId] = useState<number>();
+
+    function login(email: string | undefined, password: string | undefined) {
+        setUserId(1)
+    }
 
     return (
         <AuthContext.Provider
             value={{
+                login,
                 userId,
-                signed: false
+                signed: userId != null
             }}>
             {children}
         </AuthContext.Provider>
