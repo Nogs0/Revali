ada0d4ba0b0348aebaf4cbbc0989a3e1d637ab57
Raffaleite <Rafaelleite.comp@gmail.com>
2024-09-09 01:16:15 -0300

    correção de autenticação do token expirado

--- web/revali/src/services/api.ts
+++ web/revali/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { toast } from "sonner";
+import { useNavigate } from 'react-router-dom'
 
 export const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/api'
@@ -10,9 +11,10 @@ api.interceptors.response.use(
   (error) => {
     if (error.response && (error.response.status === 401 || error.response.status === 403)) {
       // Se o token for inválido ou a autenticação falhar
+      const navigate = useNavigate();
       localStorage.clear(); // Limpa o armazenamento local
       toast.error('Sessão expirada. Por favor, faça login novamente.'); // Mostra mensagem de erro
-      window.location.href = '/login'; // Redireciona para a página de login
+      navigate('/');
     }
     return Promise.reject(error);
   }
