ea71d383c074bdb21b7171ddeba37c194bb0e3d7
Nogs0 <joaoguinogueira04@gmail.com>
2024-09-21 18:16:45 -0300

    mend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,33 +2,13 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'paths' => ['api/*'], // Permitir CORS nas rotas que começam com /api
+    'allowed_methods' => ['*'], // Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
+    'allowed_origins' => ['*'], // Permitir requisições de qualquer origem (você pode especificar um domínio específico)
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['*'], // Permitir todos os tipos de cabeçalhos
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
