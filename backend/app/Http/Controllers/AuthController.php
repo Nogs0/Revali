86ebfd37bb0bb49c6537ceafdbb67465eb9a1250
Phablo Loureiro Alves <phabloalvesworld@gmail.com>
2024-09-23 02:48:56 -0300

    endpoint register adm banco, mudanca no tipo da coluna quantidade em itens doacao

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -21,34 +21,30 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         try {
-            // Validação dos dados de entrada
+           
             $request->validate([
                 'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users,email', 
+                'password' => 'required|string|min:6', 
+                'cpf' => 'required|string|max:14|unique:users,cpf', 
                 'pastaDeFotos' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-                'cpf' => 'required|string|max:14',
             ]);
 
-            if ($request->cpf) {
-                $existingUser = Users::where(function ($query) use ($request) {
-                    if ($request->cpf) {
-                        $query->where('cpf', $request->cpf)
-                            ->whereNotNull('cpf');
-                    }
-                })->first();
+        
+            $cpf = preg_replace('/[^0-9]/', '', $request->cpf);
 
+          
+            if ($cpf) {
+                $existingUser = Users::where('cpf', $cpf)->first();
                 if ($existingUser) {
-                    return response()->json(['message' => 'CPF ou CNPJ já registrado'], 400);
+                    return response()->json(['message' => 'CPF já registrado'], 400);
                 }
             }
 
-
-            // URL da imagem padrão
+           
             $defaultImageUrl = 'https://via.placeholder.com/150';
-
-            // Processa o upload da imagem
             if ($file = $request->file('pastaDeFotos')) {
                 $file_path = $file->getPathName();
-
                 $client = new \GuzzleHttp\Client();
                 $response = $client->request('POST', 'https://api.imgur.com/3/image', [
                     'headers' => [
@@ -59,36 +55,31 @@ class AuthController extends Controller
                         'image' => base64_encode(file_get_contents($file_path))
                     ],
                 ]);
-
                 $responseData = json_decode($response->getBody(), true);
-
                 if ($responseData['success']) {
                     $imageUrl = $responseData['data']['link'];
                 } else {
-                    return response()->json(['message' => 'Image upload failed'], 500);
+                    return response()->json(['message' => 'Falha no upload da imagem'], 500);
                 }
             } else {
-                $imageUrl = $defaultImageUrl; // Usa a imagem padrão se nenhuma for enviada
+                $imageUrl = $defaultImageUrl;
             }
 
-            // Cria o usuário
             $user = Users::create([
                 'name' => $request->name,
-                'email' => $request->cpf,
-                'password' => Hash::make($request->cpf),
-                'cpf' => $request->cpf,
-                'tipo' => 2,
+                'email' => $request->email, 
+                'password' => Hash::make($request->password), 
+                'tipo' => $request->tipo,
                 'pastaDeFotos' => $imageUrl,
                 'banco_de_alimento_id' => $request->banco_de_alimento_id
             ]);
 
-            // Cria o doador associado ao usuário
+            
             $doador = new Doadores;
             $doador->user_id = $user->id;
             $doador->pontos = 0;
             $doador->save();
 
-            // Cria a movimentação inicial do doador
             $movimentacao = new Movimentacoes;
             $movimentacao->data = now();
             $movimentacao->pontos = 0;
@@ -98,7 +89,7 @@ class AuthController extends Controller
             $movimentacao->saldo = 0;
             $movimentacao->save();
 
-            // Gera o token JWT para o novo usuário
+           
             $token = JWTAuth::fromUser($user);
 
             return response()->json([
@@ -107,10 +98,10 @@ class AuthController extends Controller
                 'banco_de_alimento_id' => $user->banco_de_alimento_id,
             ], 201);
         } catch (ValidationException $e) {
-            return response()->json(['message' => 'Validation error', 'errors' => $e->errors()], 422);
+            return response()->json(['message' => 'Erro de validação', 'errors' => $e->errors()], 422);
         } catch (Exception $e) {
             \Log::error($e->getMessage());
-            return response()->json(['message' => 'Registration failed', 'error' => $e->getMessage()], 500);
+            return response()->json(['message' => 'Falha no registro', 'error' => $e->getMessage()], 500);
         }
     }
 
@@ -118,29 +109,29 @@ class AuthController extends Controller
     public function register_doador(Request $request)
     {
         try {
-          
+
             $request->validate([
                 'name' => 'required|string|max:255',
                 'cpf' => 'required|string|max:14',
             ]);
-    
+
             $cpf = preg_replace('/[^0-9]/', '', $request->cpf);
-    
-           
+
+
             if ($cpf) {
                 $existingUser = Users::where('cpf', $cpf)->first();
-    
+
                 if ($existingUser) {
                     return response()->json(['message' => 'CPF ou CNPJ já registrado'], 400);
                 }
             }
-    
+
             $defaultImageUrl = 'https://via.placeholder.com/150';
-    
-          
+
+
             if ($file = $request->file('pastaDeFotos')) {
                 $file_path = $file->getPathName();
-    
+
                 $client = new \GuzzleHttp\Client();
                 $response = $client->request('POST', 'https://api.imgur.com/3/image', [
                     'headers' => [
@@ -151,36 +142,36 @@ class AuthController extends Controller
                         'image' => base64_encode(file_get_contents($file_path))
                     ],
                 ]);
-    
+
                 $responseData = json_decode($response->getBody(), true);
-    
+
                 if ($responseData['success']) {
                     $imageUrl = $responseData['data']['link'];
                 } else {
                     return response()->json(['message' => 'Image upload failed'], 500);
                 }
             } else {
-                $imageUrl = $defaultImageUrl; 
+                $imageUrl = $defaultImageUrl;
             }
-    
+
             $user = Users::create([
                 'name' => $request->name,
                 'email' => $cpf,
                 'cpf' => $cpf,
-                'password' => Hash::make($cpf), 
+                'password' => Hash::make($cpf),
                 'pastaDeFotos' => $imageUrl,
                 'tipo' => 2,
                 'banco_de_alimento_id' => $request->banco_de_alimento,
                 'primeiro_acesso' => 1,
             ]);
-    
-            
+
+
             $doador = new Doadores;
             $doador->user_id = $user->id;
             $doador->pontos = 0;
             $doador->save();
-    
-          
+
+
             $movimentacao = new Movimentacoes;
             $movimentacao->data = now();
             $movimentacao->pontos = 0;
@@ -189,10 +180,10 @@ class AuthController extends Controller
             $movimentacao->doador_id = $doador->id;
             $movimentacao->saldo = 0;
             $movimentacao->save();
-    
-            
+
+
             return response()->json([
-                'email' => $cpf, 
+                'email' => $cpf,
                 'senha' => $cpf,
             ], 201);
         } catch (ValidationException $e) {
@@ -202,7 +193,7 @@ class AuthController extends Controller
             return response()->json(['message' => 'Registration failed'], 500);
         }
     }
-    
+
 
     public function login(Request $request)
     {
